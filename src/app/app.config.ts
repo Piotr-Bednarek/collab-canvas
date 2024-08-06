93767a51aeac400c1b970285829b7641e8e25729
Piotr-Bednarek <72524343+Piotr-Bednarek@users.noreply.github.com>
2024-08-07 01:05:02 +0200

    fixed ci/cd pipeline

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,14 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { firebaseConfig } from '../environments/environment';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideFirebaseApp(() => initializeApp(firebaseConfig)),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
         provideAuth(() => getAuth()),
         provideFirestore(() => getFirestore()),
         provideAnimationsAsync(),
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCepsCmmncQVu60ofV6vPvmRXYYML3FNV8',
+        authDomain: 'collabcanvas-d4338.firebaseapp.com',
+        projectId: 'collabcanvas-d4338',
+        storageBucket: 'collabcanvas-d4338.appspot.com',
+        messagingSenderId: '630374173288',
+        appId: '1:630374173288:web:d3c517aacc7a136579c9dd',
+    },
+};
